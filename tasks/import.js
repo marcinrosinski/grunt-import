2c9c292fc2ad80b0b27519ebf4ec3b05e418cb02
Marcin <marcin@feedoptimise.com>
2013-11-09 13:17:18 +0000

    Update import.js
    Credits for array_unique function moved under Credits file

--- tasks/import.js
+++ tasks/import.js
@@ -28,21 +28,6 @@ module.exports = function(grunt) {
 
         var array_unique = function(inputArr) 
         {
-          // http://kevin.vanzonneveld.net
-          // +   original by: Carlos R. L. Rodrigues (http://www.jsfromhell.com)
-          // +      input by: duncan
-          // +   bugfixed by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
-          // +   bugfixed by: Nate
-          // +      input by: Brett Zamir (http://brett-zamir.me)
-          // +   bugfixed by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
-          // +   improved by: Michael Grier
-          // +   bugfixed by: Brett Zamir (http://brett-zamir.me)
-          // %          note 1: The second argument, sort_flags is not implemented;
-          // %          note 1: also should be sorted (asort?) first according to docs
-          // *     example 1: array_unique(['Kevin','Kevin','van','Zonneveld','Kevin']);
-          // *     returns 1: {0: 'Kevin', 2: 'van', 3: 'Zonneveld'}
-          // *     example 2: array_unique({'a': 'green', 0: 'red', 'b': 'green', 1: 'blue', 2: 'red'});
-          // *     returns 2: {a: 'green', 0: 'red', 1: 'blue'}
           var key = '',
             tmp_arr2 = {},
             val = '';
